15308df1aa52653e63a8f8ce352a247e15d84e2b
Carlos Valeriano <carlosvaleriano45@yahoo.com>
2023-11-15 18:09:44 -0600

    Add project files.

---
+++ TextRPGBackend/Controllers/GameController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TextRPGBackend.Controllers
+{
+
+    //Used for handling game interactions
+    [ApiController]
+    [Route("[controller]")]
+    public class GameController : Controller
+    {
+        private readonly GameInstructionService _instructionService;
+
+        //Instance created in startup, passed using dep. injection
+        public GameController(GameInstructionService instructionService)
+        {
+            _instructionService = instructionService;
+        }
+
+        [HttpPost("sendprompt")]
+        public async Task<IActionResult> SendPrompt([FromBody] string playerPrompt)
+        {
+            //process instruction and interact with AI service
+            
+        }
+
+        [HttpGet("instructions")]
+        public IActionResult GetInstructions()
+        {
+            var instructions = _instructionService.GetGameInstructions();
+            return Ok(instructions);
+        }
+    }
+}
---
+++ TextRPGBackend/Services/GameInstructionsService.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+public class GameInstructionService
+{
+    private readonly string _filePath;
+
+    public GameInstructionService()
+    {
+        // Adjust the path as per your project structure
+        _filePath = "../Config/GameInstructions.txt";
+    }
+
+    public string GetGameInstructions()
+    {
+        // Read and return the content of the file
+        return File.ReadAllText(_filePath);
+    }
+}
